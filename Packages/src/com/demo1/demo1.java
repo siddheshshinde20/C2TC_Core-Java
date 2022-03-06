37859fdc42c94a505305621caef8cbf99dc769f4
siddhesh.shinde <siddhesh.shinde@sh-lap-4598.STARHEALTH.IN>
2022-03-07 01:20:44 +0530

    first commit

---
+++ Packages/src/com/demo1/demo1.java
@@ -0,0 +1,11 @@
+package com.demo1;
+
+public class demo1 {
+	public int i=10; 
+	public void show() {
+		System.out.println("In show method");
+	}
+	
+	
+}
+ 
---
+++ Packages/src/com/demo2/demo2.java
@@ -0,0 +1,13 @@
+package com.demo2;
+
+import com.demo1.demo1;
+
+public class demo2 {
+	 public static void main (String args[]) {
+		 demo1 obj=new demo1();
+		 obj.show();
+		 System.out.println(obj.i);
+		 }
+	 }
+
+
